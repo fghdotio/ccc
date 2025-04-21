c7c085a6214ab6c1dac69138b907e112bba09bfa
fgh_ssh <fghdotio@gmail.com>
2025-04-21 12:46:56 -0500

    feat: add btc mainnet script info, support custom network

--- packages/rgbpp/src/configs/scripts/index.ts
+++ packages/rgbpp/src/configs/scripts/index.ts
@@ -1,17 +1,20 @@
 import { ccc } from "@ckb-ccc/shell";
 
 import { PredefinedNetwork } from "../../types/network.js";
+import { mainnetCellDeps, mainnetScripts } from "./mainnet.js";
 import { signetCellDeps, signetScripts } from "./signet.js";
 import { testnet3CellDeps, testnet3Scripts } from "./testnet3.js";
 
 export const predefinedScripts = {
   [PredefinedNetwork.BitcoinSignet]: signetScripts,
   [PredefinedNetwork.BitcoinTestnet3]: testnet3Scripts,
+  [PredefinedNetwork.BitcoinMainnet]: mainnetScripts,
 };
 
 export const predefinedCellDeps = {
   [PredefinedNetwork.BitcoinSignet]: signetCellDeps,
   [PredefinedNetwork.BitcoinTestnet3]: testnet3CellDeps,
+  [PredefinedNetwork.BitcoinMainnet]: mainnetCellDeps,
 };
 
 export const deadLock = ccc.Script.from({
